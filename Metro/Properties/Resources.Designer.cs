9ef3a1dcdb066a69de808acd166bd35ed9fc89c7
gemilepus <31269075+gemilepus@users.noreply.github.com>
2022-04-26 15:23:31 +0800

    v1.2.4

--- Metro/Properties/Resources.Designer.cs
+++ Metro/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Metro.Properties {
     // 類別透過 ResGen 或 Visual Studio 這類工具。
     // 若要加入或移除成員，請編輯您的 .ResX 檔，然後重新執行 ResGen
     // (利用 /str 選項)，或重建您的 VS 專案。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,7 +61,7 @@ namespace Metro.Properties {
         }
         
         /// <summary>
-        ///   查詢類似 v1.2.3 的當地語系化字串。
+        ///   查詢類似 v1.2.4 的當地語系化字串。
         /// </summary>
         internal static string Version {
             get {
