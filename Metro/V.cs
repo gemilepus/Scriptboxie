4815d4736445ffe7092f12b17a205499ca6968fa
gemilepus <31269075+gemilepus@users.noreply.github.com>
2022-04-27 10:10:20 +0800

    Update

---
+++ Metro/V.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using WindowsInput.Native;
+using System.Collections;
+using System.Windows.Forms;
+
+public class V
+{
+    public static string[] Get_Split(string CommandData)
+    {
+
+        string[] Split;
+        Split = CommandData.Split(',');
+
+        return Split;
+    }
+
+    public static string[] Get_EventValue(SortedList mSortedList, string Event)
+    {
+
+        string[] EventValue = null;
+        // Check Key
+        if (mSortedList.IndexOfKey(Event) != -1)
+        {
+            EventValue = mSortedList.GetByIndex(mSortedList.IndexOfKey(Event)).ToString().Split(',');
+        }
+
+        return EventValue;
+    }
+}
