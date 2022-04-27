0c92e0c35ff119aaa8205d80a5a27ac1f9eb12b5
gemilepus <31269075+gemilepus@users.noreply.github.com>
2022-04-27 13:05:26 +0800

    Update

--- Metro/SettingHelper.cs
+++ Metro/SettingHelper.cs
@@ -1,14 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IniParser;
+using IniParser.Model;
 
 namespace Metro
 {
     public class SettingHelper
     {
 
+        public SettingHelper()
+        {
+            var parser = new FileIniDataParser();
+            IniData data = new IniData();
+            try
+            {
+                data = parser.ReadFile("user.ini");
+            }
+            catch
+            {
+                parser.WriteFile("user.ini", new IniData());
+            }
 
+            // From location
+            if (data["Def"]["x"] == null)
+            {
+                data["Def"]["x"] = "0";
+                data["Def"]["y"] = "0";
+            }
+
+            //  WindowTitle setting
+            if (data["Def"]["WindowTitle"] == null)
+            {
+                data["Def"]["WindowTitle"] = "";
+            }
+
+            //  Script
+            if (data["Def"]["Script"] == null)
+            {
+                data["Def"]["Script"] = "";
+            }
+
+            // ScaleX, ScaleY, OffsetX, OffsetY
+            if (data["Def"]["ScaleX"] == null
+                && data["Def"]["ScaleY"] == null
+                && data["Def"]["OffsetX"] == null
+                && data["Def"]["OffsetY"] == null)
+            {
+                data["Def"]["ScaleX"] = "1";
+                data["Def"]["ScaleY"] = "1";
+                data["Def"]["OffsetX"] = "0";
+                data["Def"]["OffsetY"] = "0";
+              
+            }
+
+            parser.WriteFile("user.ini", data);
+
+        }
+
+        public void End(int x)
+        {
+
+
+        }
     }
 }
