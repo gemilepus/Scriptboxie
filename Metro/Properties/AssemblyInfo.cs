658c3cc31ac2182582a6c08ecc5129517680319d
gemilepus <31269075+gemilepus@users.noreply.github.com>
2023-03-29 11:21:43 +0800

    v1.3.16

--- Metro/Properties/AssemblyInfo.cs
+++ Metro/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.3.15")]
+[assembly: AssemblyFileVersion("1.3.16")]
